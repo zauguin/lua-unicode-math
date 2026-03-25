2cfb29195c2621ab963967cc00131fc6fcc556aa
Marcel Fabian Krüger <tex@2krueger.de>
2026-03-25 23:20:02 +0100

    Assign mathclass for \Upsilon

--- lua-unicode-math.lua
+++ lua-unicode-math.lua
@@ -84,10 +84,12 @@ end})
 -- We derive mathclasses from the Unicode data file MathClass-15.txt, but some characters can be used
 -- in different ways in math and LaTeX traditionally defines a different class for them by default than
 -- Unicode does. For these we adjust the class here.
+-- Also Unicode does not consider Upsilon to be a math character...
 mathclasses[0x2F] = 'N' -- /
 mathclasses[0x5C] = 'N' -- \
 mathclasses[0x22EF] = 'N' -- ⋯
 mathclasses[0x2E] = 'N' -- .
+mathclasses[0x3A5] = 'A' -- Υ
 
 -- Integrals and other big operators have the same classes in data files, but we need to tell them apart.
 -- Therefore we have a fixed list of all integral like codepoints here.
